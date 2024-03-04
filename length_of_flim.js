fb15c9386eb9bd94490786844604ddc6686df081
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-03-04 20:11:30 +0100

    minutes

---
+++ length_of_flim.js
@@ -0,0 +1,7 @@
+
+const LengthInMinutes = 223;
+
+const LengthInHours = Math.floor(LengthInMinutes/60)
+const LengthInMinutesRest = LengthInMinutes %60
+console.log(LengthInHours)
+console.log(LengthInMinutesRest)
